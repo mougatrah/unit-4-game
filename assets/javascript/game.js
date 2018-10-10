1d560a80fb927f8ed47afe4a68d79f29baa957cb
workstation1@DOMAIN.LOCAL <workstation1@DOMAIN.LOCAL>
2018-10-10 16:46:42 -0700

    added images and got character select and attacking set up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,44 +7,66 @@ var game = {
         name : "Luke Skywalker",
         hp : 100,
         attack : 5,
-        counter : 5
+        counter : 5,
+        div: $("#0hp")
     },{
         name : "Obi-Wan Kenobi",
         hp : 120,
         attack : 8,
-        counter : 8
+        counter : 8,
+        div: $("#1hp")
     },{
         name : "Darth Sidious",
         hp : 150,
         attack : 20,
-        counter : 20
+        counter : 20,
+        div: $("#2hp")
     },{
         name : "Darth Maul",
         hp : 180,
         attack : 25,
-        counter : 25
+        counter : 25,
+        div: $("#3hp")
     }],
- 
-    setPlayer(id){
-        console.log("setting player as " + id.name);
-      
+
+    setup(){
+        for(let dude in this.characters){
+            this.characters[dude].div.text(this.characters[dude].hp);
+        }
     },
-    setDefender(){
-        console.log("setting defender");
-        this.state.defender = this.characters[this.id];
+
+    setChar(){
+        if(game.state.player === null){
+            game.state.player = game.characters[this.id];
+            console.log("setting player as " + game.characters[this.id].name);
+            $("#enemyRow").append($(".character").css("background-color", "red"));
+            $("#playerRow").append($(this).css("background-color", "white"));
+        }else if(game.state.player != null && game.state.defender === null){
+            console.log("setting defender as " + game.characters[this.id].name);
+            game.state.defender = game.characters[this.id];
+            $("#defenderRow").append(this);
+        }
     },
 
     attack(){
-        if(this.state.player && this.state.defender){
-            console.log("You did " + this.state.player.attack.toString() +" damage.");
-            console.log(this.state.defender.name + " did " + this.state.defender.counter.toString() + " damage to you.");
-            this.state.defender.hp -= this.state.player.attack;
-            this.state.player.hp -= this.state.defender.counter;
-            this.state.player.attack += this.state.player.counter;
+        if(game.state.player && game.state.defender){
+            console.log("You did " + game.state.player.attack +" damage.");
+            console.log(game.state.defender.name + " did " + game.state.defender.counter + " damage to you. ");
+            game.state.defender.hp -= game.state.player.attack;
+            game.state.player.hp -= game.state.defender.counter;
+            game.state.player.attack += game.state.player.counter;
+
+            game.state.player.div.text(game.state.player.hp);
+            game.state.defender.div.text(game.state.defender.hp);
+        }
+
+        if(game.state.defender.hp <= 0){
+            game.state.defender.hp
         }
     }
 }
 
-$(".character").on("click", {name: "yekl"} ,game.setPlayer);
+$(".character").on("click", game.setChar); 
+$("#attack").on("click", game.attack);            
 
-
+game.setup();
