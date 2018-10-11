8f076e32e089ad1290f7e6a78bd6ab10f413774d
workstation1@DOMAIN.LOCAL <workstation1@DOMAIN.LOCAL>
2018-10-10 17:59:53 -0700

    reset working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,34 +4,51 @@ var game = {
         defender : null
     },
     characters : [{
-        name : "Luke Skywalker",
+        name : "Luke Skywalker", 
+        defaultHp: 100,  
         hp : 100,
         attack : 5,
         counter : 5,
-        div: $("#0hp")
+        div: $("#0"),
+        hpText: $("#0hp")
     },{
         name : "Obi-Wan Kenobi",
+        defaultHp: 120,  
         hp : 120,
         attack : 8,
         counter : 8,
-        div: $("#1hp")
+        div: $("#1"),
+        hpText: $("#1hp")
     },{
         name : "Darth Sidious",
+        defaultHp: 150,  
         hp : 150,
         attack : 20,
         counter : 20,
-        div: $("#2hp")
+        div: $("#2"),
+        hpText: $("#2hp")
     },{
         name : "Darth Maul",
+        defaultHp: 180,  
         hp : 180,
         attack : 25,
         counter : 25,
-        div: $("#3hp")
+        div: $("#3"),
+        hpText: $("#3hp")
     }],
 
     setup(){
-        for(let dude in this.characters){
-            this.characters[dude].div.text(this.characters[dude].hp);
+
+        game.state.player = null;
+        game.state.defender = null;
+
+        for(let dude in game.characters){
+            $("#topRow").append(game.characters[dude].div.css("background-color", "gray"));
+            game.characters[dude].attack = game.characters[dude].counter;
+            game.characters[dude].hp = game.characters[dude].defaultHp;
+            game.characters[dude].hpText.text(game.characters[dude].hp);
+            game.characters[dude].div.show();
+
         }
     },
 
@@ -41,7 +58,7 @@ var game = {
             console.log("setting player as " + game.characters[this.id].name);
             $("#enemyRow").append($(".character").css("background-color", "red"));
             $("#playerRow").append($(this).css("background-color", "white"));
-        }else if(game.state.player != null && game.state.defender === null){
+        }else if(game.state.player != null && game.state.defender === null &&  game.characters[this.id] != game.state.player){
             console.log("setting defender as " + game.characters[this.id].name);
             game.state.defender = game.characters[this.id];
             $("#defenderRow").append(this);
@@ -56,17 +73,23 @@ var game = {
             game.state.player.hp -= game.state.defender.counter;
             game.state.player.attack += game.state.player.counter;
 
-            game.state.player.div.text(game.state.player.hp);
-            game.state.defender.div.text(game.state.defender.hp);
+            game.state.player.hpText.text(game.state.player.hp);
+            game.state.defender.hpText.text(game.state.defender.hp);
+
+            if(game.state.defender.hp <= 0){
+                game.state.defender.div.hide();
+                game.state.defender = null;
+            }
+            if(game.state.player.hp <= 0){
+                game.setup();
+            }
         }
 
-        if(game.state.defender.hp <= 0){
-            game.state.defender.hp
-        }
+        
     }
 }
 
 $(".character").on("click", game.setChar); 
 $("#attack").on("click", game.attack);            
-
+$("#reset").on("click", game.setup);
 game.setup();
